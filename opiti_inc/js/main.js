6d2593678ce9741794d933600e29ccb1fbaea48d
Opiti Calvin <opiticalvin@gmail.com>
2020-08-15 12:56:32 +0200

    updated posting of message from jquery to fetch api

--- opiti_inc/js/main.js
+++ opiti_inc/js/main.js
@@ -322,23 +322,33 @@
 
 		// console.log(messageData);
 
-		$.post({
-			url: "https://solutions.opiticonsulting.com/api/messages",
-			data: JSON.stringify(messageData),
-			contentType: "application/json",
-			success: function (data, status) {
+		fetch("https://solutions.opiticonsulting.com/api/messages",{
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			mode: 'cors',
+			method: 'POST',
+			body: JSON.stringify(messageData)
+		})
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			if (data.code === 200){
 				$("#msgForm")[0].reset();
 				flash(data.message);
-			},
-			error: function (xhr, status, error){
-				let errorMessage = xhr.statusText + ": Message not sent!";
-				// console.log(errorMessage);
-				// console.log(error);
-				flash(errorMessage, {
+			} else {
+				// $("#msgForm")[0].reset();
+				flash(data.message, {
 					"bgColor": "#C0392B"
 				});
 			}
-		});
+			
+		})
+		.catch((error)=> {
+			flash(error, {
+				"bgColor": "#C0392B"
+			});
+		})
 	});
 
 
